f305222ec4b39810142b447e7f5d947c133b1771
FlorijanDem <florijan.dem@gmail.com>
2025-02-27 14:54:28 +0300

    Add try/catch and additional SQL query

--- backend/dbCreating.js
+++ backend/dbCreating.js
@@ -1,11 +1,18 @@
 const { sql } = require("./dbConnection");
 
 const createDBtables = async () => {
-  // Create users table if not exists
-  // Max password length by validation rules 20
-  // Role do not must be so long
-  // Can not say anything about username and email long so 255  
-  await sql`
+  try {
+    // This is SQL query disable notices 
+    // (like table already exists) inside createDBtables function
+    await sql`
+          SET client_min_messages TO WARNING;
+      `;
+
+    // Create users table if not exists
+    // Max password length by validation rules 20
+    // Role do not must be so long
+    // Can not say anything about username and email long so 255
+    await sql`
         CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
             username VARCHAR(255) UNIQUE NOT NULL,
@@ -13,10 +20,11 @@ const createDBtables = async () => {
             password VARCHAR(20) NOT NULL,
             email VARCHAR(255) UNIQUE NOT NULL
     );`;
-  // Create characteristics table who refences to user id
-  // Some characteristics can be optionall, 
-  // no information about this point in jira task
-  await sql`
+
+    // Create characteristics table who refences to user id
+    // Some characteristics can be optionall,
+    // no information about this point in jira task
+    await sql`
         CREATE TABLE IF NOT EXISTS characteristics (
             user_id INTEGER REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
             height FLOAT,
@@ -25,10 +33,11 @@ const createDBtables = async () => {
             gender VARCHAR(25)
         )
     `;
-  // Create recipes table
-  // The "type" field represents the recipe category, such as "Vegetarian" or "Vegan".
-  // If no category it remains NULL. 
-  await sql`
+
+    // Create recipes table
+    // The "type" field represents the recipe category, such as "Vegetarian" or "Vegan".
+    // If no category it remains NULL.
+    await sql`
         CREATE TABLE IF NOT EXISTS recipes (
             id SERIAL PRIMARY KEY,
             title TEXT NOT NULL,
@@ -38,14 +47,18 @@ const createDBtables = async () => {
             products INTEGER []
         )
     `;
-  // Create products table, 
-  // every product have own id who putted into "recipes.products" array
-  await sql`
+
+    // Create products table,
+    // every product have own id who putted into "recipes.products" array
+    await sql`
         CREATE TABLE IF NOT EXISTS products (
             id INTEGER PRIMARY KEY,
             title TEXT NOT NULL
         )
     `;
+  } catch (err) {
+    console.error("Failed to create tables:", err);
+  }
 };
 
 module.exports = { createDBtables };
