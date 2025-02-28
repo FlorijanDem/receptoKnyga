979cc58546d70900028633a31b003de7cbd0699b
FlorijanDem <florijan.dem@gmail.com>
2025-02-28 14:15:52 +0300

    Change component name

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { registerUser } = require("../controllers/userController");
 const validate = require("../validators/validate");
-const { checkRegisterBody } = require("../validators/checkBody");
+const { checkRegisterBody } = require("../validators/checkRegisterBody");
 
 const authRouter = express.Router();
 
