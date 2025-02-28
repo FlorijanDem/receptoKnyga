0fed252950225840836f52d04fecf0cb0c944638
Arturas Mickevicius <arturas.mickevicius@stud.techin.lt>
2025-02-28 10:42:56 +0400

    Add few coments to recipe model

--- backend/models/recipeModel.js
+++ backend/models/recipeModel.js
@@ -7,7 +7,7 @@ exports.getAllRecipes = async (filter) => {
 
   // Needs more work on filtering
   const recipes = await sql`
-    SELECT recipes.*, products.*
+    SELECT recipes.*
     FROM recipes
     JOIN recipes_products
     ON recipes.id = recipes_products.recipe_id
@@ -23,16 +23,40 @@ exports.getAllRecipes = async (filter) => {
 };
 
 exports.getRecipeById = async (id) => {
-  const recipe = await sql`
+  // Needs refinment when DB is ready
+  const recipe = await sql.begin(async () => {
+    const [recipe] = await sql`
     SELECT *
     FROM recipes
     WHERE id = ${id}
     `;
 
+    const productIDs = await sql`
+    SELECT product_id
+    FROM recipes_products
+    WHERE recipe_id = ${recipe.id}
+    `;
+
+    recipe.products = await Promise.all(
+      productIDs.map(async ({ product_id }) => {
+        const [product] = await sql`
+          SELECT *
+          FROM products
+          WHERE id = ${product_id}
+          `;
+
+        return product;
+      })
+    );
+
+    return recipe;
+  });
+
   return recipe;
 };
 
 exports.createRecipe = async (recipe) => {
+  // Needs refinement when DB is ready
   const newRecipe = await sql`
     INSERT INTO recipes ${sql(recipe)}
     RETURNING *
@@ -44,6 +68,7 @@ exports.createRecipe = async (recipe) => {
 exports.updateRecipe = async (id, data) => {
   const columns = Object.keys(data);
 
+  //   Needs refinement when DB is ready
   const updatedRecipe = await sql`
     UPDATE recipes
     SET ${sql(data, columns)}
