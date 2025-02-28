675f975d9cdb0a944962879c70c7cb3966d42bc1
FlorijanDem <florijan.dem@gmail.com>
2025-02-28 13:48:10 +0300

    Add JWT token send after register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,26 @@
 const AppError = require("../utils/appError");
 const { registerUser } = require("../models/userModel");
-
+const jwt = require("jsonwebtoken");
 const argon2 = require("argon2");
+
+const signToken = (id) => {
+  const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+    algorithm: "HS256",
+  });
+
+  return token;
+};
+
+const sendCookie = (token, res) => {
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  res.cookie("jwt", token, cookieOptions);
+};
 
 exports.registerUser = async (req, res, next) => {
   try {
@@ -9,6 +28,9 @@ exports.registerUser = async (req, res, next) => {
     const hashedPassword = await argon2.hash(newUser.password);
     newUser.password = hashedPassword;
     const user = await registerUser({ newUser });
+
+    const token = signToken(user);
+    sendCookie(token, res);
 
     user.id = undefined;
     user.password = undefined;
