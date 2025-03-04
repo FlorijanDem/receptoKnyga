626181b380ed31f62fc30c926256b9a9243e7ff6
Arturas Mickevicius <arturas.mickevicius@stud.techin.lt>
2025-03-04 13:55:31 +0400

    Move styles to index.css

--- frontend/src/components/RecipePreviewCard.jsx
+++ frontend/src/components/RecipePreviewCard.jsx
@@ -3,30 +3,26 @@ import { IoPeople } from "react-icons/io5";
 
 const RecipePreviewCard = ({ recipe }) => {
   return (
-    <article className="recipe-preview-card w-[300px]  text-gray-600 rounded-lg bg-white overflow-hidden">
-      <div className="recipe-preview-card__image-container w-full h-[200px]">
+    <article className="recipe-preview-card">
+      <div className="recipe-preview-card__image-container">
         <img
-          className="recipe-preview-card__image object-cover w-full"
+          className="recipe-preview-card__image"
           src={recipe.photo}
           alt={recipe.title}
         />
       </div>
-      <div className="recipe-preview-card__info flex gap-4 mt-2 px-4">
-        <span className="flex items-center gap-1">
-          {recipe?.calories || 400} Cal
-        </span>
-        <span className="flex items-center gap-1">
+      <div className="recipe-preview-card__info">
+        <span>{recipe?.calories || 400} Cal</span>
+        <span>
           <LuClock4 /> {recipe?.preparation_time} Min
         </span>
-        <span className="flex items-center gap-1">
+        <span>
           <IoPeople /> {recipe?.servings} People
         </span>
       </div>
-      <h2 className="recipe-preview-card__title text-2xl text-gray-800 font-bold mb-2 px-4">
-        {recipe?.title}
-      </h2>
-      <hr className="recipe-preview-card__line mx-4" />
-      <p className="recipe-preview-card__type mb-3 px-4">{recipe?.type}</p>
+      <h2 className="recipe-preview-card__title">{recipe?.title}</h2>
+      <hr className="recipe-preview-card__line" />
+      <p className="recipe-preview-card__type">{recipe?.type}</p>
     </article>
   );
 };
--- frontend/src/components/RecipesList.jsx
+++ frontend/src/components/RecipesList.jsx
@@ -2,9 +2,9 @@ import RecipePreviewCard from "./RecipePreviewCard";
 
 const RecipesList = ({ recipes }) => {
   return (
-    <section className="recipes-list-container py-10 bg-gray-200 ">
+    <section className="recipes-list-container">
       <h1>Recipes List</h1>
-      <div className="recipes-list w-max mx-auto flex flex-col gap-5 items-center md:grid md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4">
+      <div className="recipes-list">
         {recipes?.length === 0 && <p>No recipes found</p>}
         {recipes.map((recipe) => (
           <RecipePreviewCard key={recipe.id} recipe={recipe} />
