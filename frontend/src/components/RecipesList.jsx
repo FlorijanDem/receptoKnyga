916948e1b6d785152105289458f41a093b4ef34b
Arturas Mickevicius <arturas.mickevicius@stud.techin.lt>
2025-03-04 13:39:11 +0400

    Add grid layout for larger screens

--- frontend/src/components/RecipesList.jsx
+++ frontend/src/components/RecipesList.jsx
@@ -2,12 +2,14 @@ import RecipePreviewCard from "./RecipePreviewCard";
 
 const RecipesList = ({ recipes }) => {
   return (
-    <section className="recipes-list bg-gray-200 flex flex-col gap-5 items-center">
+    <section className="recipes-list-container py-10 bg-gray-200 ">
       <h1>Recipes List</h1>
-      {recipes?.length === 0 && <p>No recipes found</p>}
-      {recipes.map((recipe) => (
-        <RecipePreviewCard key={recipe.id} recipe={recipe} />
-      ))}
+      <div className="recipes-list w-max mx-auto flex flex-col gap-5 items-center md:grid md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4">
+        {recipes?.length === 0 && <p>No recipes found</p>}
+        {recipes.map((recipe) => (
+          <RecipePreviewCard key={recipe.id} recipe={recipe} />
+        ))}
+      </div>
     </section>
   );
 };
