85d0f05c8febf7fb9bb27b63cb764adf17be0b4e
[Nedas Jurk] <[dead.nedas@gmail.com]>
2025-03-05 11:14:41 +0200

    fix responsive design

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -45,7 +45,7 @@ const Navigation = () => {
       <div className="flex flex-col md:flex-row items-center mx-[1.563rem] mt-[2rem] mb-[2rem]">
         <div className="flex justify-between items-center w-full md:w-auto">
           <Link to="/">
-            <h1 className="font-jakarta text-recipe-primary font-bold tracking-[-1px] text-[24px] md:text-[32px] md:ml-[3.75rem]">
+            <h1 className="font-jakarta text-recipe-primary font-bold tracking-[-1px] text-[24px] md:text-[32px] 2xl:text-[60px] 2xl:ml-[3.75rem]">
               Calibrium
             </h1>
           </Link>
@@ -53,24 +53,31 @@ const Navigation = () => {
             <img src={NavMenuIcon} alt="menu Icon" width="24" height="24" />
           </button>
         </div>
-        <div className="md:max-w-lg w-full mt-4 flex md:mt-0 md:ml-[4rem] search-bar-container items-center flex-grow">
+
+        <div className="2xl:max-w-[1000px] md:max-w-[600px] w-full mt-4 flex md:mt-0 md:ml-[4rem] search-bar-container items-center flex-grow md:pr-[2rem]">
           <SearchBar />
           <div className="ml-[1rem] mr-[1.563rem]">
             <button className="">
               <img
                 src={NavFilterIcon}
                 alt="Filter icon"
-                className="w-12 h-12"
+                className="w-[3rem] h-[3rem] 2xl:w-[6rem] 2xl:h-[6rem]"
               />
             </button>
           </div>
         </div>
 
-        <div className="hidden md:flex md:ml-auto md:justify-end md:mr-[3.75rem] desktop-icons flex-shrink-0">
+        <div className="hidden md:flex md:ml-auto md:justify-end md:mr-[3.75rem]  desktop-icons flex-shrink-0">
           {icons.map((icon, index) => (
             <Link to={icon.path} key={index}>
               <button className={iconStyle}>
-                <img src={icon.src} alt={icon.alt} width="44" height="44" />
+                <img
+                  src={icon.src}
+                  alt={icon.alt}
+                  width="44"
+                  height="44"
+                  className="2xl:w-[5rem] 2xl:h-[5rem]"
+                />
               </button>
             </Link>
           ))}
