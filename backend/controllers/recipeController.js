122a67d3f83b561ca9c3137a5c5b10bf61097440
Arturas Mickevicius <arturas.mickevicius@stud.techin.lt>
2025-03-05 10:20:06 +0400

    Add basic pagination in frontend and Fix getAllRecipes in backend

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -9,16 +9,16 @@ const {
 
 exports.getAllRecipesHandler = async (req, res, next) => {
   try {
-    const searchString = Object.entries(req.query)
-      .map(([key, value]) => `${key} ILIKE '%${value}%'`)
-      .join(" AND ");
+    // const searchString = Object.entries(req.query)
+    //   .map(([key, value]) => `${key} ILIKE '%${value}%'`)
+    //   .join(" AND ");
 
     const filter = {
-      limit: req.query.limit || 12,
-      offset: (req.query.page - 1) * req.query.limit || 0,
+      limit: +req.query.limit || 12,
+      offset: (+req.query.page - 1) * +req.query.limit || 0,
       sortBy: req.query.sortBy || "title",
       order: req.query.order || "asc",
-      searchString,
+      // searchString,
     };
 
     const recipes = await getAllRecipes(filter);
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,55 +1,11 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useErrorBoundary } from "react-error-boundary";
 import RecipesList from "./RecipesList";
 import SomethingAboutMacros from "./SomethingAboutMacros";
 
-const API_URL = import.meta.env.VITE_API_URL;
-
 const Home = () => {
-  const [recipes, setRecipes] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const { showBoundary } = useErrorBoundary();
-
-  useEffect(() => {
-    const fetchRecipes = async () => {
-      try {
-        const { data: response } = await axios.get(`${API_URL}/recipes`, {
-          withCredentials: true,
-        });
-
-        setRecipes(response.data);
-        setError(null);
-        setLoading(false);
-      } catch (error) {
-        setLoading(false);
-        if (axios.isAxiosError(error)) {
-          if (error.response) {
-            setError(error.response.data.message);
-          } else if (error.request) {
-            setError("Something went wrong. Please try again later.");
-          } else {
-            setError("Network error. Please check your internet connection.");
-          }
-        } else {
-          showBoundary(error);
-        }
-      }
-    };
-
-    fetchRecipes();
-  }, []);
   return (
     <>
       <SomethingAboutMacros />
-      {loading ? (
-        <p>...Loading</p>
-      ) : error ? (
-        <p>{error}</p>
-      ) : (
-        <RecipesList recipes={recipes} />
-      )}
+      <RecipesList />
     </>
   );
 };
