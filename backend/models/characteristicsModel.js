ea8e976fc1d6b6c2fb64dbedd9a1cd1f2ec50217
FlorijanDem <florijan.dem@gmail.com>
2025-03-06 14:21:38 +0300

    Add true patch

--- backend/models/characteristicsModel.js
+++ backend/models/characteristicsModel.js
@@ -11,30 +11,31 @@ exports.getCharacteristicsById = async (id) => {
 
 exports.updateCharacteristic = async (data, id) => {
   // Check if the user characteristics exist
-  const user = await sql`
+  const existingUser = await sql`
       SELECT *
       FROM characteristics
       WHERE user_id=${id}
   `;
 
-  if (user.length === 0) {
+  if (existingUser.length === 0) {
     // Insert new characteristics if no existing record is found
 
     const [characteristics] = await sql`
         INSERT INTO characteristics (user_id, height, weight, age, gender)
-        VALUES (${id}, ${data.height}, ${data.weight}, ${data.age}, ${data.gender})
+        VALUES (${id}, ${data.height ?? null}, ${data.weight ?? null}, ${data.age ?? null}, ${data.gender ?? null})
         RETURNING characteristics.*
     `;
     return characteristics;
   } else {
+    const user = existingUser[0];
     // Update existing characteristics
     const [characteristic] = await sql`
           UPDATE characteristics
           SET 
-            height = ${data.height},
-            weight = ${data.weight},
-            age = ${data.age},
-            gender = ${data.gender}
+            height = ${data.height ?? user.height ?? null},
+            weight = ${data.weight ?? user.weight ?? null},
+            age = ${data.age ?? user.age ?? null},
+            gender = ${data.gender ?? user.gender ?? null}
           WHERE user_id=${id}
           RETURNING *
     `;
