c19b1caf41a276122ca7a37353aec3129a38c246
FlorijanDem <florijan.dem@gmail.com>
2025-03-06 10:23:00 +0300

    Remove unneedeble functions

--- backend/controllers/characteristicsController.js
+++ backend/controllers/characteristicsController.js
@@ -1,22 +1,8 @@
 const {
-  getCharacteristics,
   getCharacteristicsById,
-  postCharacteristic,
   updateCharacteristic,
-  deleteCharacteristic,
 } = require("../models/characteristicsModel");
 
-exports.getUserCharacteristicsAll = async (req, res, next) => {
-  try {
-    const characteristics = await getCharacteristics();
-    res.status(200).json({
-      status: "success",
-      data: characteristics,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
 
 exports.getUserCharacteristicsMy = async (req, res, next) => {
   try {
@@ -35,21 +21,6 @@ exports.getUserCharacteristicsMy = async (req, res, next) => {
   }
 };
 
-exports.postCharacteristic = async (req, res, next) => {
-  try {
-    const characteristics = await postCharacteristic({
-      ...req.body,
-      user_id: req.user?.id,
-    });
-
-    res.status(201).json({
-      status: "success",
-      data: characteristics,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
 
 exports.updateCharacteristic = async (req, res, next) => {
   try {
@@ -64,15 +35,3 @@ exports.updateCharacteristic = async (req, res, next) => {
   }
 };
 
-exports.deleteCharacteristic = async (req, res, next) => {
-  try {
-    const characteristic = await deleteCharacteristic(req.user?.id);
-
-    res.status(200).json({
-      status: "success",
-      data: characteristic,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
--- backend/models/characteristicsModel.js
+++ backend/models/characteristicsModel.js
@@ -1,13 +1,5 @@
 const { sql } = require("../dbConnection");
 
-exports.getCharacteristics = async () => {
-  const characteristics = await sql`
-        SELECT *
-        FROM characteristics
-    `;
-  return characteristics;
-};
-
 exports.getCharacteristicsById = async (id) => {
   const [characteristic] = await sql`
         SELECT *
@@ -17,25 +9,10 @@ exports.getCharacteristicsById = async (id) => {
   return characteristic;
 };
 
-exports.postCharacteristic = async (data) => {
-  const [characteristic] = await sql`
-        INSERT INTO characteristics ${sql(data, ["user_id", "height", "weight", "age", "gender"])} 
-        RETURNING characteristics.*`;
-  return characteristic;
-};
-
 exports.updateCharacteristic = async (data, id) => {
   const [characteristic] = await sql`
         UPDATE characteristics
         SET ${sql(data)}
-        WHERE user_id=${id}
-        RETURNING characteristics.*`;
-  return characteristic;
-};
-
-exports.deleteCharacteristic = async (id) => {
-  const [characteristic] = await sql`
-        DELETE FROM characteristics
         WHERE user_id=${id}
         RETURNING characteristics.*`;
   return characteristic;
