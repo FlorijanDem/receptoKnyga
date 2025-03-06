ca7663c0538fb6b16cac54c643495bd36d667019
FlorijanDem <florijan.dem@gmail.com>
2025-03-06 13:49:14 +0300

    Remove message and fix error

--- backend/controllers/characteristicsController.js
+++ backend/controllers/characteristicsController.js
@@ -3,24 +3,21 @@ const {
   updateCharacteristic,
 } = require("../models/characteristicsModel");
 
-
 exports.getUserCharacteristicsMy = async (req, res, next) => {
   try {
     const characteristics = await getCharacteristicsById(req.user?.id);
     if (characteristics === undefined) {
-      res.status(404).json({
-        message: "You do not have characteristics",
+      res.status(404).json({});
+    } else {
+      res.status(200).json({
+        status: "success",
+        data: characteristics,
       });
     }
-    res.status(200).json({
-      status: "success",
-      data: characteristics,
-    });
   } catch (error) {
     next(error);
   }
 };
-
 
 exports.updateCharacteristic = async (req, res, next) => {
   const data = req.body;
@@ -36,4 +33,3 @@ exports.updateCharacteristic = async (req, res, next) => {
     next(error);
   }
 };
-
