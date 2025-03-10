4d83dda136f98ee0f6602d3ec5c5b63505f0e6a0
Arturas Mickevicius <arturas.mickevicius@stud.techin.lt>
2025-03-10 13:19:25 +0200

    Fix getMe function to hide password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -128,6 +128,8 @@ exports.getMe = async (req, res, next) => {
       res.status(200).json({ user: null });
     }
 
+    user.password = undefined;
+
     res.status(200).json({ user });
   } catch (err) {
     next(new AppError(err.message, 401));
