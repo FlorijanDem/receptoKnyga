a49c74274449e99276c6f395ea06fd9dea2ca57e
FlorijanDem <florijan.dem@gmail.com>
2025-03-14 17:18:40 +0300

    Add warnings and errors counters

--- backend/envChecker.js
+++ backend/envChecker.js
@@ -2,27 +2,43 @@ const isNaString = (value) => typeof value !== "string";
 const validateEnv = () => {
   console.log("Enviroment messages:");
 
+  let warnings = 0;
+  let errors = 0;
+  
   if (!process.env.PORT) {
     console.log("\x1b[33m", "PORT not found, using default port 3001");
     process.env.PORT = 3001;
+    warnings++;
   } else if (isNaN(process.env.PORT)) {
     console.log("\x1b[31m", "PORT must be number");
+    errors++;
   }
 
   if (!process.env.DB_PORT) {
     console.log("\x1b[33m", "DB_PORT not found, using default port 5432");
     process.env.DB_PORT = 5432;
+    warnings++;
   } else if (isNaN(process.env.DB_PORT)) {
     console.log("\x1b[31m", "DB_PORT must be number");
+    errors++;
   }
 
   if (!process.env.DB_NAME) {
     console.log("\x1b[33m", "DB_PORT not found, using default name postgres");
     process.env.DB_NAME = "postgres";
+    warnings++;
   } else if (isNaString(process.env.DB_NAME)) {
     console.log("\x1b[31m", "DB_NAME must be string");
+    errors++;
   }
-  // Reset color 
+
+  if (warnings == 0 && errors == 0) {
+    console.log("\x1b[32m", "You do not have any warnings or errors");
+  } else {
+    console.log("\x1b[33m", `You have ${warnings} warnings`);
+    console.log("\x1b[31m", `You have ${errors} errors`);
+  }
+  // Reset color
   console.log("\x1b[0m");
 };
 
