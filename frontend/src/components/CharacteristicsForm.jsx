2f885ff23464f63b8e419202531b3d64133fc4a0
FlorijanDem <florijan.dem@gmail.com>
2025-03-14 10:29:23 +0300

    Add characteristics form

---
+++ frontend/src/components/CharacteristicsForm.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
+
+const CharacteristicsForm = () => {
+  const [error, setError] = useState(null);
+  const [data, setData] = useState({
+    height: "",
+    weight: "",
+    age: "",
+    gender: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.patch(`${API_URL}/characteristics`, data, {
+        withCredentials: true,
+      });
+      setError(null);
+      console.log("Success:", response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response) {
+          setError(error.response.data.message);
+        } else if (error.request) {
+          setError("Something went wrong. Please try again later.");
+        } else {
+          setError("Network error. Please check your internet connection.");
+        }
+      } else {
+        setError(error);
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-4">
+      <input
+        type="number"
+        name="height"
+        value={data.height}
+        onChange={handleChange}
+        placeholder="Height"
+        className="input"
+      />
+      <input
+        type="number"
+        name="weight"
+        value={data.weight}
+        onChange={handleChange}
+        placeholder="Weight"
+        className="input"
+      />
+      <input
+        type="number"
+        name="age"
+        value={data.age}
+        onChange={handleChange}
+        placeholder="Age"
+        className="input"
+      />
+      <select
+        name="gender"
+        value={data.gender}
+        onChange={handleChange}
+        className="input"
+      >
+        <option value="">Select Gender</option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="other">Other</option>
+      </select>
+      <button type="submit" className="btn btn-primary">
+        Update
+      </button>
+      {error && <p className="text-red-500">{error}</p>}
+    </form>
+  );
+};
+
+export default CharacteristicsForm;
