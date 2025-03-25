d9cb9f93c9db3c40a4f57c4189a47c6890678e4b
Arturas Mickevicius <arturas.mickevicius@stud.techin.lt>
2025-03-25 08:54:45 +0200

    Change userController to send user.id with user data

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,7 +37,7 @@ exports.registerUser = async (req, res, next) => {
     const token = signToken(user.id);
     sendCookie(token, res);
 
-  //  user.id = undefined;
+    // user.id = undefined;
     user.password = undefined;
 
     res.status(201).json({
@@ -54,7 +54,7 @@ exports.loginUser = async (req, res, next) => {
 
   try {
     const user = await getUserByEmail(email);
-    
+
     if (!user) {
       return res.status(401).json({
         message: "Invalid email or password",
@@ -77,7 +77,7 @@ exports.loginUser = async (req, res, next) => {
     const token = signToken(user.id);
     sendCookie(token, res);
 
-  //  user.id = undefined;
+    // user.id = undefined;
     user.password = undefined;
 
     res.status(200).json({
@@ -147,6 +147,9 @@ exports.updateUser = async (req, res, next) => {
     const data = req.body;
     const id = req.params.id;
     const user = await updateUser(data, id);
+
+    user.password = undefined;
+
     res.status(200).json({ status: "success", data: user });
   } catch (err) {
     next(new AppError(err.message, 401));
