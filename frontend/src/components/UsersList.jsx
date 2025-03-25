72553c1b30f2fc2f61e898bba1a77394b2553a0d
Arturas Mickevicius <arturas.mickevicius@stud.techin.lt>
2025-03-25 14:50:39 +0200

    Add admin navigation

--- frontend/src/components/UsersList.jsx
+++ frontend/src/components/UsersList.jsx
@@ -1,7 +1,3 @@
-// <<<<<<< HEAD
-// <<<<<<< HEAD
-// =======
-// >>>>>>> a092213 (Add users list)
 import { useEffect, useContext, useState } from "react";
 import UserContext from "../contexts/UserContext";
 import { useNavigate } from "react-router";
@@ -56,52 +52,6 @@ const UsersList = () => {
       ))}
     </section>
   );
-  // =======
-  // import { useEffect } from "react";
-  // import { useLocation } from "react-router";
-  // =======
-
-  // const API_URL = import.meta.env.VITE_API_URL;
-  // >>>>>>> a092213 (Add users list)
-
-  // const UsersList = () => {
-  //   const { user } = useContext(UserContext);
-  //   const navigate = useNavigate();
-
-  //   const [users, setUsers] = useState([]);
-  //   useEffect(() => {
-  //     if (user?.role !== "admin") {
-  //       navigate("/");
-  //       return;
-  //     }
-
-  // <<<<<<< HEAD
-  //   return <h1>UsersList</h1>;
-  // >>>>>>> 05b6ea2 (Add admin navigation)
-  // =======
-  //     const fetchUsers = async () => {
-  //       try {
-  //         const { data: response } = await axios.get(`${API_URL}/users`, {
-  //           withCredentials: true,
-  //         });
-
-  //         setUsers(response.data);
-  //       } catch (error) {
-  //         console.log(error);
-  //       }
-  //     };
-  //     fetchUsers();
-  //   }, []);
-
-  //   return (
-  //     <>
-  //       <h1>UsersList</h1>
-  //       {users.map((user) => (
-  //         <div key={user.id}>{user.username}</div>
-  //       ))}
-  //     </>
-  //   );
-  // >>>>>>> a092213 (Add users list)
 };
 
 export default UsersList;
