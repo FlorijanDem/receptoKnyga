56a21ce22aaba2e860bc8f1a1b1e92c31eae4d49
Arturas Mickevicius <arturas.mickevicius@stud.techin.lt>
2025-03-26 12:42:30 +0200

    Add ban/unban user functionality to userCard

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -1,12 +1,41 @@
+import axios from "axios";
+import { useState } from "react";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
 const UserCard = ({ user }) => {
+  const [currentUser, setCurrentUser] = useState(user);
+  const banUser = async () => {
+    try {
+      const { data: response } = await axios.patch(
+        `${API_URL}/users/${currentUser.id}`,
+        { banned: !currentUser.banned },
+        { withCredentials: true }
+      );
+
+      console.log(response);
+
+      setCurrentUser(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="user-card my-2 mx-auto w-full bg-[var(--color-recipe-fifth)] py-2 px-4 rounded-xl">
-      <p>Username: {user.username}</p>
-      <p className="text-[var(--color-recipe-secondary)]">{user.email}</p>
+      <p>Username: {currentUser.username}</p>
+      <p className="text-[var(--color-recipe-secondary)]">
+        {currentUser.email}
+      </p>
       <p>Role: {user.role}</p>
-      {user.banned && (
+      {currentUser.banned && (
         <p className="text-[var(--color-recipe-fourth)]">Banned</p>
       )}
+      <button
+        onClick={banUser}
+        className="cursor-pointer bg-[var(--color-recipe-fourth)] p-2 rounded-md text-[var(--color-recipe-fifth)]"
+      >
+        {currentUser.banned ? "Unban" : "Ban"}
+      </button>
     </div>
   );
 };
