6bc707ef38b13dde0810c5d6c6816146e6d7a652
ArturasDz <arturasdzim10@gmail.com>
2025-03-26 13:08:22 +0200

    Update Router

--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -6,6 +6,8 @@ const {
   getMe,
   protect,
   updatePassword,
+  forgotPassword,
+  resetPassword,
 } = require("../controllers/userController");
 const validate = require("../validators/validate");
 const { checkRegisterBody } = require("../validators/checkRegisterBody");
@@ -18,5 +20,7 @@ authRouter.route("/login").post(checkLoginBody, validate, loginUser);
 authRouter.route("/logout").post(logout);
 authRouter.route("/me").get(getMe);
 authRouter.route("/update-password").patch(protect, updatePassword);
+authRouter.route("/forgot-password").post(forgotPassword);
+authRouter.route("/reset-password/:token").post(resetPassword);
 
 module.exports = authRouter;
