5adac5fea2a598cbf5c248674bc56e6eee592e96
Arturas Mickevicius <arturas.mickevicius@stud.techin.lt>
2025-03-26 12:29:27 +0200

    Change admin navigation radio inputs

--- frontend/src/components/AdminNavigation.jsx
+++ frontend/src/components/AdminNavigation.jsx
@@ -20,6 +20,8 @@ const AdminNavigation = () => {
     navigate(`${adminPage === "recipes" ? "/" : `/${adminPage}`}`);
   };
 
+  console.log(window.location.pathname);
+
   useEffect(() => {
     reset();
     if (user?.role !== "admin") {
