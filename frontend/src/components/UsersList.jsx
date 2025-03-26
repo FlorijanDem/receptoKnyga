a092213360b131f8a3fb37c66943b743ef6f553e
Arturas Mickevicius <arturas.mickevicius@stud.techin.lt>
2025-03-26 10:40:22 +0200

    Add users list

--- frontend/src/components/UsersList.jsx
+++ frontend/src/components/UsersList.jsx
@@ -1,15 +1,43 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router";
+import { useEffect, useContext, useState } from "react";
+import UserContext from "../contexts/UserContext";
+import { useNavigate } from "react-router";
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 const UsersList = () => {
-  const location = useLocation();
-  console.log("location.state", location.state);
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
 
+  const [users, setUsers] = useState([]);
   useEffect(() => {
-    console.log(location.state);
-  }, [location.state]);
+    if (user?.role !== "admin") {
+      navigate("/");
+      return;
+    }
 
-  return <h1>UsersList</h1>;
+    const fetchUsers = async () => {
+      try {
+        const { data: response } = await axios.get(`${API_URL}/users`, {
+          withCredentials: true,
+        });
+
+        setUsers(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <>
+      <h1>UsersList</h1>
+      {users.map((user) => (
+        <div key={user.id}>{user.username}</div>
+      ))}
+    </>
+  );
 };
 
 export default UsersList;
