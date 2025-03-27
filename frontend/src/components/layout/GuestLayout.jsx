4c68a193d66197c759cc5c38d8c17c3458d1bb97
D3JS <d3js@TECHIN.INTERNAL>
2025-03-27 14:06:53 +0400

    Add to the GuestLayout

--- frontend/src/components/layout/GuestLayout.jsx
+++ frontend/src/components/layout/GuestLayout.jsx
@@ -3,6 +3,7 @@ import Login from "../Login";
 import Register from "../Register";
 import WelcomePage from "../WelcomePage";
 import { Routes, Route } from "react-router";
+import ForgotPassword from "../ForgotPassword";
 function GuestLayout() {
   return (
     <>
@@ -10,6 +11,7 @@ function GuestLayout() {
         <Route path="/" element={<WelcomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
       <Footer/>
     </>
