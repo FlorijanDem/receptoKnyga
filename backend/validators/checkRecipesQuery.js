ed811433c89e0a6b8a269753a3cbc78cdb78110a
ArturasDz <arturasdzim10@gmail.com>
2025-04-10 19:00:50 +0300

    Fix checkRecipesQuery because I kept getting Order must be either ASC or DESC and didn't understand why

--- backend/validators/checkRecipesQuery.js
+++ backend/validators/checkRecipesQuery.js
@@ -60,9 +60,9 @@ exports.checkRecipeQuery = [
   query("order")
     .optional()
     .trim()
-    .toUpperCase()
-    .isIn(["ASC", "DESC"])
-    .withMessage("Order must be either ASC or DESC"),
+    .toLowerCase()
+    .isIn(["asc", "desc", "new", "old", "title"])
+    .withMessage("Order must be either new, old, or title"),
 
   // Užtikriname, kad nėra papildomų nenumatytų parametrų
   checkExact([], {
