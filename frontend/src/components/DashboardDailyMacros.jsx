79142b6d7c229e5e17a9334331bdf517032c73b9
dominyka dobuzinskaite <dominyka.dobuzinskaite@gmail.com>
2025-04-17 10:59:35 +0300

    Dashboard update

--- frontend/src/components/DashboardDailyMacros.jsx
+++ frontend/src/components/DashboardDailyMacros.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { format } from "date-fns";
 import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
-
+import { differenceInYears } from "date-fns";
+import { calculateAllMetrics } from "../utils/calculator";
 const API_URL = import.meta.env.VITE_API_URL;
 
 const DashboardDailyMacros = ({ selectedDate, refreshKey }) => {
@@ -14,6 +15,62 @@ const DashboardDailyMacros = ({ selectedDate, refreshKey }) => {
   });
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [result, setResult] = useState(null);
+  const [activityLevels, setActivityLevels] = useState();
+  const [characteristicsData, setCharacteristicsData] = useState({
+    height: "",
+    weight: "",
+    age: "",
+    date_of_birth: "",
+    gender: "",
+    activity_level_id: "",
+    my_goals: "",
+  });
+
+  const fetchActivityLevels = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/activity`, {
+        withCredentials: true,
+      });
+      setActivityLevels(response.data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const fetchCharacteristics = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/characteristics`, {
+        withCredentials: true,
+      });
+      const { user_id: _, ...filteredData } = response.data.data;
+
+      setCharacteristicsData({
+        ...filteredData,
+        age: differenceInYears(
+          new Date(),
+          new Date(filteredData.date_of_birth)
+        ),
+        weight: filteredData.weightHistory[0].weight,
+        // activityLevel: filteredData.activity_level_id, // Initialize with default activity level
+      });
+      console.log(characteristicsData);
+      const metrics = calculateAllMetrics(
+        parseFloat(characteristicsData.weight),
+        parseFloat(characteristicsData.height),
+        parseInt(characteristicsData.age),
+        characteristicsData.gender,
+        activityLevels.find(
+          (level) => level.id === +characteristicsData.activity_level_id
+        ).multiplier || 1,
+        String(characteristicsData.my_goals)
+      );
+
+      setResult(metrics);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const fetchDailyMacros = async () => {
     setIsLoading(true);
@@ -47,6 +104,16 @@ const DashboardDailyMacros = ({ selectedDate, refreshKey }) => {
   useEffect(() => {
     fetchDailyMacros();
   }, [selectedDate, refreshKey]);
+
+  useEffect(() => {
+    fetchActivityLevels();
+    fetchCharacteristics();
+  }, [userMacros]);
+
+  useEffect(() => {
+    fetchActivityLevels();
+    fetchCharacteristics();
+  }, []);
 
   const pieData = [
     { name: "Fats", value: userMacros.fat, color: "#0D3559" },
@@ -102,6 +169,7 @@ const DashboardDailyMacros = ({ selectedDate, refreshKey }) => {
               </PieChart>
             </ResponsiveContainer>
           </div>
+
           <div className="daily-macros__details">
             <p>
               <span
@@ -127,6 +195,44 @@ const DashboardDailyMacros = ({ selectedDate, refreshKey }) => {
           </div>
         </div>
       )}
+      <div className="bg-[var(--color-recipe-fifth)] rounded-lg mb-4 shadow-md p-4 flex flex-col items-center">
+        {(() => {
+          const calorieDifference =
+            Number(result?.dailyCalories.toFixed(0)) -
+            Math.round(userMacros.calories);
+
+          if (calorieDifference <= 0) {
+            return (
+              <>
+                <p className="text-red-600 font-semibold">
+                  You've reached your daily calorie goal.
+                </p>
+                <p className="text-base text-gray-500">
+                  Limit exceeded by{" "}
+                  <span className=" text-red-500 font-bold text-lg">
+                    {Math.abs(calorieDifference)}
+                  </span>{" "}
+                  calories.
+                </p>
+              </>
+            );
+          } else {
+            return (
+              <>
+                <p className="text-blue-800 font-bold text-lg">
+                  You've still got some calories left for today!
+                </p>
+                <p className="text-base text-gray-500">
+                  Remaining calories:{" "}
+                  <span className="font-semibold text-blue-800">
+                    {Math.abs(calorieDifference)}
+                  </span>{" "}
+                </p>
+              </>
+            );
+          }
+        })()}
+      </div>
     </div>
   );
 };
